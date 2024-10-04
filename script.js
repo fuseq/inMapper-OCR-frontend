6b5524967ff0927623271180694ecc504c87b78c
Furkan Şenoğlu <20190808076@ogr.akdeniz.edu.tr>
2024-10-04 10:49:30 +0300

    .

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 stableTimer = setTimeout(() => {
                     takePictureAndSend();
                     statusText.innerText = "Fotoğraf çekildi!";
-                }, 3000); // 2 saniye boyunca sabitse fotoğraf çek
+                }, 2000); // 2 saniye boyunca sabitse fotoğraf çek
             }
         } else {
             isStable = false;
